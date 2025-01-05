6b83688bc07b053c9def0ddabe23170df435cea7
Oktambek2003 <116434960+oktambek2003@users.noreply.github.com>
2025-01-05 09:11:55 +0500

    card qismi tayyor

--- lib/features/shop/screens/card/card_widget.dart
+++ lib/features/shop/screens/card/card_widget.dart
@@ -1,17 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:iconsax/iconsax.dart';
 import 'package:onlinemarket/common/widget/appbar/appbar.dart';
-import 'package:onlinemarket/common/widget/icon/circular_icon.dart';
-import 'package:onlinemarket/common/widget/images/onlineshopRounted_images.dart';
 import 'package:onlinemarket/common/widget/product/cart/add_remove_buttom.dart';
 import 'package:onlinemarket/common/widget/product/cart/cart_item.dart';
 import 'package:onlinemarket/common/widget/product/priceText/price_text.dart';
-import 'package:onlinemarket/common/widget/texts/brand_title_text_vefield_icon.dart';
-import 'package:onlinemarket/common/widget/texts/product_title.dart';
-import 'package:onlinemarket/utils/constants/colors.dart';
-import 'package:onlinemarket/utils/constants/images_string.dart';
 import 'package:onlinemarket/utils/constants/sizes.dart';
-import 'package:onlinemarket/utils/helpers/helper_function.dart';
 
 class onlineShopCardScreen extends StatelessWidget {
   const onlineShopCardScreen({super.key});
